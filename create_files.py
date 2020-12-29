fae6a03a6a00beee57fff8c7fdae021a487f58be
Damien Carrele <damiencarrele@yahoo.fr>
2020-12-29 16:53:15 +0100

    return X_train, y_train

--- create_files.py
+++ create_files.py
@@ -6,12 +6,29 @@ import numpy as np
 
 
 
+SPLIT_TRAIN_TEST = 0.8
+
+parties_complete_names = {
+    'GDR': 'Gauche Démocrate et Républicaine',
+    'LaREM': 'La République en Marche',
+    'LR': 'Les Républicains',
+    'LT': 'Libertés et Territoires',
+    'FI': 'La France Insoumise',
+    'UDI-I': 'UDI et Indépendants',
+    'SOC': 'Socialistes et Apparentés',
+    'Agir ens': 'Agir Ensemble',
+    'NI': 'Non Inscrit',
+    'Dem': 'Mouvement Démocrate et Démocrates Apparentés'
+}
+
+
 def get_releveant_informations_from_json_vote(file_name):
     ''' Returns a dictionnary with all the information that shall be used in the Vote class,
         except the party votes that are processed in **another function**
     '''
     with open(file_name, 'r') as f:
         scrutin = json.load(f)['scrutin']
+
     dictionnary = {
         'id': scrutin['uid'],
         'code_type_vote': scrutin['typeVote']['codeTypeVote'],
@@ -25,16 +42,26 @@ def get_releveant_informations_from_json_vote(file_name):
 
     return dictionnary
 
-def create_json_vote_files(directory='votes', output_directory='data'):
+def create_json_vote_files(directory='votes', output_directory='data', split_train_test=SPLIT_TRAIN_TEST):
     ''' For each "vote" json file in the directory, create a json file in the output_directory
         containing relevant data for the Vote objects
     '''
     votes_names = os.listdir(directory)
     votes_names.sort(key=lambda name: int(splitext(name)[0][10:]))
+    len_train = int(split_train_test * len(votes_names))
+    if not os.path.exists('data'):
+        os.mkdir('data')
+    if not (os.path.exists('data\\train') and os.path.exists('data\\test')):
+        os.mkdir('data\\train')
+        os.mkdir('data\\test')
 
-    for f_name in votes_names:
+    for i, f_name in enumerate(votes_names):
         dictionnary = get_releveant_informations_from_json_vote(join(directory, f_name))
-        with open(join(output_directory, f_name), 'w') as f:
+        if i <= len_train:
+            actual_output_directory = join(output_directory, 'train')
+        else:
+            actual_output_directory = join(output_directory, 'test')
+        with open(join(actual_output_directory, f_name), 'w') as f:
             json.dump(dictionnary, f)
 
 def get_vote_informations_from_json_vote(scrutin):
@@ -55,7 +82,7 @@ def get_vote_informations_from_json_vote(scrutin):
 
     return votes
 
-def count_votes_per_party(file_name, input_actors_data='dpt_data/dpt_data.csv'):
+def count_votes_per_party(file_name, input_actors_data='dpt_data/liste_deputes_excel.csv'):
     ''' Returns a DataFrame indexed by parties, and with columns corresponding to
         each type of vote ('nonVotants', 'pours', 'contres', 'abstentions'), for the 
         json file corresponding to file_name
@@ -63,10 +90,18 @@ def count_votes_per_party(file_name, input_actors_data='dpt_data/dpt_data.csv'):
     with open(file_name, 'r') as f:
         scrutin = json.load(f)['scrutin']
 
-    vote_columns = ['nonVotants', 'pours', 'contres', 'abstentions', 'total_count']
+    vote_columns = ['nonVotants', 'pours', 'contres', 'abstentions']
     votes = get_vote_informations_from_json_vote(scrutin)
-    dpt_data = pd.read_csv(input_actors_data, sep=';').set_index('identifiant')
-    parties = set(dpt_data['parti_sigle'])
+    
+    # processing the dataset
+    dpt_data = pd.read_csv(input_actors_data, sep=';', encoding='utf-8')
+    if input_actors_data == 'dpt_data/liste_deputes_excel.csv':
+        dpt_data['identifiant'] = dpt_data['identifiant'].apply(lambda x: 'PA' + str(x))
+        dpt_columns = dpt_data.columns
+        dpt_data.set_index('identifiant', inplace=True)
+        dpt_data.rename(columns={dpt_columns[-2]: 'parti', dpt_columns[-1]: 'parti_sigle'}, inplace=True)
+        parties = set(dpt_data['parti_sigle'])
+
     parties_count_df = pd.DataFrame(
         np.zeros(shape=(len(parties), len(vote_columns)), dtype=int),
         columns=vote_columns,
@@ -78,21 +113,30 @@ def count_votes_per_party(file_name, input_actors_data='dpt_data/dpt_data.csv'):
             voter_party = dpt_data.loc[voter, 'parti_sigle']
         else:           # the deputy is not in the dataframe ; we don't include him/her in the ratio
             continue
-        parties_count_df.loc[voter_party, vote] += 1            
-        parties_count_df.loc[voter_party, 'total_count'] += 1
+        parties_count_df.loc[voter_party, vote] += 1
 
     return parties_count_df
 
-def create_vote_count_csv_files(directory='votes', output_directory='data'):
+def create_vote_count_csv_files(directory='votes', output_directory='data', split_train_test=SPLIT_TRAIN_TEST):
     ''' For each "vote" json file in the directory, create a csv file in the output_directory
         containing vote informations for each party (number of 'abstentions', 'pours'...)
     '''
     votes_names = os.listdir(directory)
     votes_names.sort(key=lambda name: int(splitext(name)[0][10:]))
+    len_train = int(split_train_test * len(votes_names))
+    if not os.path.exists('data'):
+        os.mkdir('data')
+    if not (os.path.exists('data\\train') and os.path.exists('data\\test')):
+        os.mkdir('data\\train')
+        os.mkdir('data\\test')
 
-    for f_name in votes_names:
+    for i, f_name in enumerate(votes_names):
         df = count_votes_per_party(directory + '/' + f_name)
-        name_export = join(output_directory, splitext(f_name)[0]) + '.csv'
+        if i <= len_train:
+            actual_output_directory = join(output_directory, 'train')
+        else:
+            actual_output_directory = join(output_directory, 'test')
+        name_export = join(actual_output_directory, splitext(f_name)[0]) + '.csv'
         df.to_csv(name_export)
 
 if __name__ == "__main__":
@@ -101,4 +145,3 @@ if __name__ == "__main__":
     print('creating csv files...')
     create_vote_count_csv_files()
     print('done.')
-
--- problem.py
+++ problem.py
@@ -12,6 +12,8 @@ DATA_HOME = "data"
 
 @dataclass
 class Vote:
+    ''' Base class containing all relevant basis information of the dataset
+    '''
     id: str
     code_type_vote: str
     libelle_type_vote: str
@@ -23,15 +25,15 @@ class Vote:
     vote_counts: pd.DataFrame
 
     @classmethod
-    def load_from_files(cls, id, data_home=DATA_HOME):
-        f_name = join(data_home, id)
+    def load_from_files(cls, id, data_home=DATA_HOME, train_or_test='train'):
+        f_name = join(data_home, train_or_test, id)
         with open(f_name + ".json", "r") as f:
             vote_metadata = json.load(f)
 
-        vote_counts = (pd.read_csv(f_name + ".csv", sep=","))
-                       #rename({'Unnamed: 0': 'parties'}).
+        vote_counts = (pd.read_csv(f_name + ".csv", sep=",").
+                       rename(columns={'Unnamed: 0': 'party'}).
                        ######## renommer la première colonne (partis)
-                       #set_index('parties'))
+                       set_index('party'))
 
         vote = cls(id=id,
                    code_type_vote=vote_metadata['code_type_vote'],
@@ -45,14 +47,50 @@ class Vote:
 
         return vote
 
+    def to_X_y(self):
+        ''' Transform a Vote object into an observation X of features (dictionnary)
+            and a label y
+        '''
+        number_of_dpt_per_party = {party: sum(self.vote_counts.loc[party])
+                                    for party in self.vote_counts.index}
+        X = {'code_type_vote': self.code_type_vote,
+             'libelle_type_vote': self.libelle_type_vote,
+             'sort': self.sort,
+             'demandeur': self.demandeur,
+             'libelle': self.libelle,
+             'nb_votants': self.nb_votants,
+             'date': self.date,
+             'presence_per_party': number_of_dpt_per_party}
+        
+        vote_columns = self.vote_counts.columns
+        y = {}
+        for party in self.vote_counts.index:
+            major_position = vote_columns[np.argmax(self.vote_counts.loc[party])]
+            y[party] = major_position
 
-votes_names = os.listdir('data')
-votes_names = [splitext(name)[0] for name in votes_names if name.endswith('.json')]
-print(votes_names[0])
-votes_names.sort(key=lambda name: int(splitext(name)[0][10:]))
-for i, name in enumerate(votes_names[:100]):
-    vote = Vote.load_from_files(name)
-    print('vote', i, ':')
-    print('demandeur :', vote.demandeur)
-    print('libelle :', vote.libelle)
-    print()
+        return X, y
+
+def _read_data(path, train_or_test='train'):
+    ''' Return the features dataset X and the labels dataset y for either the train or the test
+    '''
+    directory = join(path, DATA_HOME, train_or_test)
+    votes_names = os.listdir(directory)
+    votes_names = [splitext(vote)[0] for vote in votes_names if vote.endswith('.json')]
+    votes_names.sort(key=lambda name: int(splitext(name)[0][10:]))
+    
+    for i, f_name in enumerate(votes_names):
+        vote = Vote.load_from_files(f_name, train_or_test=train_or_test)
+        features, label = vote.to_X_y()
+        if i == 0:
+            X = pd.DataFrame(columns=[key for key in features.keys()])
+            y = pd.DataFrame(columns=[key for key in label.keys()])
+        X.loc[f_name] = features
+        y.loc[f_name] = label
+
+    return X, y
+
+def get_train_data(path='.'):
+    return _read_data(path=path, train_or_test='train')
+
+def get_test_data(path='.'):
+    return _read_data(path=path, train_or_test='test')
